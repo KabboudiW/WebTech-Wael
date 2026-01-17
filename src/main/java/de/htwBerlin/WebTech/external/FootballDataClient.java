28b19cff7df9fa1772084edccb1826332f0b11a9
KabboudiW <Wael.Kabboudi@Student.HTW-Berlin.de>
2026-01-17 22:38:05 +0100

    externalapi

--- src/main/java/de/htwBerlin/WebTech/external/FootballDataClient.java
+++ src/main/java/de/htwBerlin/WebTech/external/FootballDataClient.java
@@ -1,34 +1,28 @@
 package de.htwBerlin.WebTech.external;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestClient;
 
 @Component
 public class FootballDataClient {
 
-    private final WebClient webClient;
+    private final RestClient restClient;
 
     public FootballDataClient(@Value("${footballData.token}") String token) {
-        this.webClient = WebClient.builder()
+        this.restClient = RestClient.builder()
                 .baseUrl("https://api.football-data.org/v4")
                 .defaultHeader("X-Auth-Token", token)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 
-    public String getStandingsJson(String competitionCode) {
-        return webClient.get()
-                .uri("/competitions/{code}/standings", competitionCode)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-    }
-
     public String getScorersJson(String competitionCode) {
-        return webClient.get()
+        return restClient.get()
                 .uri("/competitions/{code}/scorers", competitionCode)
                 .retrieve()
-                .bodyToMono(String.class)
-                .block();
+                .body(String.class);
     }
 }
